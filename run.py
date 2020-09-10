64615e18b61be0cedd8eafd145badbdab4000cf3
Marinus Enzinger <marinus@enzingerm.de>
2020-09-10 08:52:49 +0200

    Add Webserver

--- run.py
+++ run.py
@@ -1,51 +1,47 @@
 import asyncio
-from html.parser import HTMLParser
+from datetime import datetime
 
 import aiohttp
-from lxml import html
 
-from config import PASS, USER
+from call_list import get_calls, login
+from sanic import Blueprint, Sanic, response
+from sanic_jinja2 import SanicJinja2
 
-LOGIN_URL = "https://www.dus.net/de/"
-CALLER_LIST = "https://www.dus.net/de/kundenmenue/kundencenter/callerlist.html"
-CUSTOMER_CENTER = "https://www.dus.net/de/kundenmenue/kundencenter/index.html"
-
-
-async def login(session):
-    data = None
-    async with session.get(LOGIN_URL) as response:
-        parsed = html.fromstring((await response.text()).encode())
-        data = {it.name: it.value for it in parsed.xpath(
-            "//form[@id='login-form']//input[@type='hidden']")}
-    data['username'] = USER
-    data['password'] = PASS
-    async with session.post(LOGIN_URL, data=data) as response:
-        pass
-    session.cookie_jar.update_cookies(
-        [('DUSNETCookiesConsent', '{"time":1599591086762,"list":[null,{"id":1,"name":"Funktionelle Cookies","type":"required","agree":1},{"id":2,"name":"Analytische Cookies","type":"optional","agree":1}]}')])
-    # strangely, kundencenter must be loaded first
-    async with session.get(CUSTOMER_CENTER) as response:
-        pass
-    anrufe = []
-    async with session.get(CALLER_LIST) as response:
-        parsed = html.fromstring((await response.text()).encode())
-        for i in parsed.xpath("//div[text()='verpasste Anrufe']/following-sibling::table/tr[position()>1]"):
-            datum, nummer = i.xpath("td/text()")
-            anrufe.append(('eingehend', datum, nummer, None))
-        for i in parsed.xpath("//div[text()='getätigte Anrufe']/following-sibling::table/tr[position()>1]"):
-            datum, nummer, dauer = i.xpath("td/text()")
-            anrufe.append(('ausgehend', datum, nummer, dauer))
-        for i in parsed.xpath("//div[text()='angenommene Anrufe']/following-sibling::table/tr[position()>1]"):
-            datum, nummer, dauer = i.xpath("td/text()")
-            anrufe.append(('eingehend', datum, nummer, dauer))
-
-    print(anrufe)
+app = Sanic(__name__)
+jinja = SanicJinja2(app)
+call_list = []
+last_updated = datetime.now()
 
 
-async def main():
+@app.route("/calls")
+@jinja.template("call_list.jinja2")
+async def calls(request):
+    return {"last_updated": last_updated.strftime('%H:%M'), "calls": call_list}
+
+
+async def update_calls():
+    global call_list, last_updated
     async with aiohttp.ClientSession() as session:
         await login(session)
+        while True:
+            print("Updating calls...")
+            call_list = await get_calls(session)
+            print("updated calls!")
+            last_updated = datetime.now()
+            await asyncio.sleep(60)
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    server = app.create_server(
+        host='127.0.0.1',
+        port=8000,
+        return_asyncio_server=True
+    )
+    loop.create_task(server)
+    loop.create_task(update_calls())
+    loop.run_forever()
+
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    main()
